3af5686534edc48d87925ba7e24a737a5d1060eb
Omar AbdElaty <140123457+OmarLucky102@users.noreply.github.com>
2026-01-26 22:47:34 +0200

    Refactor tour template to use dynamic data, mixins, and formatted date

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,8 +13,20 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) get the data for the requested tour (include the tour guide)
+  console.log(req.params.slug);
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+
+  // 2) Build themplate
+
+  // 3) Render template Using the data form step 1
+
+  res.status(200).render('tour', {
+    title: req.params.slug,
+    tour,
+  });
+});
